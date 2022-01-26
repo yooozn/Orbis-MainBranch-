8e5b4c12a9caf1b489b58fd8bac8b52d05a8bef8
yooozn <yooozn@github.com>
2022-01-26 13:57:35 -0800

    end of day

--- Brock_Bosses/Mackenzie/MackenzieBoss.gd
+++ Brock_Bosses/Mackenzie/MackenzieBoss.gd
@@ -8,6 +8,7 @@ onready var _animPlayer = $AnimationPlayer
 var rng = RandomNumberGenerator.new()
 onready var projectile1 = preload("res://Brock_Bosses/Mackenzie/MackenzieProjectile.tscn")
 onready var projectile2 = preload("res://Brock_Bosses/Mackenzie/MackenzieProjectile2.tscn")
+onready var projectile3 = preload("res://Brock_Bosses/Mackenzie/MackenzieProjectile3.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_animPlayer.play("idle")
@@ -37,12 +38,12 @@ func attack1():
 
 func attack2():
 	_animPlayer.play("attack2")
-	yield(get_tree().create_timer(1),"timeout")
+	yield(get_tree().create_timer(3),"timeout")
 	attack()
 
 func attack3():
 	_animPlayer.play("attack3")
-	yield(get_tree().create_timer(1),"timeout")
+	yield(get_tree().create_timer(3),"timeout")
 	attack()
 
 func _on_DetectionArea_body_entered(body):
@@ -75,3 +76,22 @@ func _projectileAttack2():
 	var proj5 = projectile2.instance()
 	proj5.position = global_position + Vector2(600,rng.randi_range(-300,-350))
 	get_owner().add_child(proj5)
+
+func _projectileAttack3():
+	var proj = projectile3.instance()
+	proj.position = global_position + Vector2(-850, -50)
+	get_owner().add_child(proj)
+	var proj2 = projectile3.instance()
+	proj2.position = global_position + Vector2(-600, -200)
+	get_owner().add_child(proj2)
+	var proj3 = projectile3.instance()
+	proj3.position = global_position + Vector2(-400, -350)
+	get_owner().add_child(proj3)
+	var proj4 = projectile3.instance()
+	proj4.position = global_position + Vector2(-200, -500)
+	get_owner().add_child(proj4)
+	var proj5 = projectile3.instance()
+	proj5.position = global_position + Vector2(0, -650)
+	get_owner().add_child(proj5)
+#MAKE the first projectile attack that goes vertical go up as she flies up, and then when she reaches her peak make the projectiles fly down again
+#Make another projectile attack that goes on a diagonal line path, similar to vertical one but diagonal
